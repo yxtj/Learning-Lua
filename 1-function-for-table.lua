28b4abd5bf8a58b885fa654022d6943236492f5f
yanxiangtianji <yanxiangtianji@gmail.com>
2015-02-03 16:49:50 -0500

    add empty lines to seperate output result.

--- 1-function-for-table.lua
+++ 1-function-for-table.lua
@@ -7,13 +7,17 @@ end
 --table (array)
 a={11,22,33,44,55};
 show(a);
+print();
 
 --table (is a C++ map indeed)
 a.id=1;
 a.name='yanxiangtianji'
 show(a);
+print();
+
 a[3]='haha';
 show(a)
+print();
 
 print('index is start from 1: a[1]=',a[1]);
 
